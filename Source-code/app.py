3fe45482f40f2d44d9808d18f6abdad8fd32b72b
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-02-10 18:02:27 -0300

    versao inicial

--- Source-code/app.py
+++ Source-code/app.py
@@ -5,44 +5,31 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-
-def checkPostedData(postedData, functionName):
-    if (functionName == "add"):
-        if "x" not in postedData or "y" not in postedData:
-            return 301
-        else:
-            return 200
 class Add(Resource):
     def post(self):
-        #If I am here, then the resouce Add was requested using the method POST
 
-        #Step 1: Get posted data:
         postedData = request.get_json()
-
-        #Steb 1b: Verify validity of posted data
-        status_code = checkPostedData(postedData, "add")
-        if (status_code!=200):
-            retJson = {
-                "Message": "An error happened",
-                "Status Code":status_code
-            }
-            return jsonify(retJson)
-
-        #If i am here, then status_code == 200
         x = postedData["x"]
         y = postedData["y"]
         x = int(x)
         y = int(y)
+        ret = x + y
 
-        #Step 2: Add the posted data
-        ret = x+y
         retMap = {
-            'Message': ret,
-            'Status Code': 200
+            'Sum' : ret
+            'Status code' : 200
         }
-        return jsonify(retMap)
 
-class Subtract(Resource):
+    return jsonify(retMap)
+
+
+    def get(self):
+
+    def put(self):
+
+
+
+class Substract(Resource):
     pass
 
 class Multiply(Resource):
